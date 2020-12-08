6ee01d13eadf5d8924964b1d527cd4e229615b82
LittleSec <517862788@qq.com>
2020-12-08 17:11:30 +0800

    json: add 3rd party: nlohmann/json.hpp

---
+++ include/README.md
@@ -0,0 +1,48 @@
+# third party
+
+## [nlohmann/json.hpp](https://github.com/nlohmann/json/releases/tag/v3.9.1)
+1. test code
+```cpp
+#include "nlohmann/json.hpp"
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <string>
+
+using json = nlohmann::json;
+
+int testJson(std::vector<int> vec, const std::string &s) {
+  // create an empty structure (null)
+  json j;
+  // add a number that is stored as double (note the implicit conversion of j to
+  // an object)
+  j["pi"] = 3.141;
+  // add a Boolean that is stored as bool
+  j["happy"] = true;
+  // add a string that is stored as std::string
+  j["name"] = "Niels";
+  // add another null object by passing nullptr
+  j["nothing"] = nullptr;
+  // add an object inside the object
+  j["answer"]["everything"] = 42;
+  // add an array that is stored as std::vector (using an initializer list)
+  j["list"] = {1, 0, 2};
+  // add another object (using an initializer list of pairs)
+  j["object"] = {{"currency", "USD"}, {"value", 42.99}};
+  std::cout << std::setw(4) << j << std::endl;
+
+  // instead, you could also write (which looks very similar to the JSON above)
+  json j2 = {{"pi", 3.141},
+             {"happy", true},
+             {"name", s},
+             {"nothing", vec},
+             {"answer", {{"everything", 42}}},
+             {"object", {{"currency", "USD"}, {"value", 42.99}}}};
+  // j2["list"] = vec;
+  std::cout << std::setw(4) << j2 << std::endl;
+
+  return 0;
+}
+
+// testJson({10, 30, 20}, ts);
+```
--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+#include "nlohmann/json.hpp"
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
 #include "ASTManager.h"
 
 #include <llvm-c/Target.h>
@@ -6,8 +11,42 @@
 
 using namespace lksast;
 using namespace clang;
+using json = nlohmann::json;
 
 #define USAGE "Usage: ./bin astList.txt\n"
+
+int testJson(std::vector<int> vec, const std::string &s) {
+  // create an empty structure (null)
+  json j;
+  // add a number that is stored as double (note the implicit conversion of j to
+  // an object)
+  j["pi"] = 3.141;
+  // add a Boolean that is stored as bool
+  j["happy"] = true;
+  // add a string that is stored as std::string
+  j["name"] = "Niels";
+  // add another null object by passing nullptr
+  j["nothing"] = nullptr;
+  // add an object inside the object
+  j["answer"]["everything"] = 42;
+  // add an array that is stored as std::vector (using an initializer list)
+  j["list"] = {1, 0, 2};
+  // add another object (using an initializer list of pairs)
+  j["object"] = {{"currency", "USD"}, {"value", 42.99}};
+  std::cout << std::setw(4) << j << std::endl;
+
+  // instead, you could also write (which looks very similar to the JSON above)
+  json j2 = {{"pi", 3.141},
+             {"happy", true},
+             {"name", s},
+             {"nothing", vec},
+             {"answer", {{"everything", 42}}},
+             {"object", {{"currency", "USD"}, {"value", 42.99}}}};
+  // j2["list"] = vec;
+  std::cout << std::setw(4) << j2 << std::endl;
+
+  return 0;
+}
 
 int main(int argc, char *argv[]) {
   LLVMInitializeNativeTarget();
@@ -15,29 +54,31 @@ int main(int argc, char *argv[]) {
 
   if (argc < 2) {
     llvm::errs() << USAGE;
-    return 1;
+    std::string ts = "Join";
+    return testJson({10, 30, 20}, ts);
+    // return 1;
+  } else if (argc == 2) {
+    std::string astFiletxt = argv[1];
+    ASTManager manager(astFiletxt);
+    for (auto fd : manager.getFunctionDecls()) {
+      llvm::errs() << fd->getNameInfo().getAsString() << ": ";
+      SourceManager &sm = fd->getASTContext().getSourceManager();
+      llvm::errs() << fd->getLocation().printToString(sm) << "\n\t";
+      bool flag1 = fd->doesThisDeclarationHaveABody();
+      bool flag2 = fd->isDefined();
+      llvm::errs() << "doesThisDeclarationHaveABody: " << flag1
+                   << ", isDefined: " << flag2 << "\n";
+    }
+    llvm::errs() << "direct function call: " << manager.getDirectCallCnt()
+                 << "\n";
+    llvm::errs() << "var function ptr call: " << manager.getVarFPCnt() << "\n";
+    llvm::errs() << "parm var function ptr call: " << manager.getParmVarFPCnt()
+                 << "\n";
+    llvm::errs() << "field var function ptr call: "
+                 << manager.getFieldVarFPCnt() << "\n";
+  } else {
+    llvm::errs() << "TODO\n";
   }
-
-  std::string astFiletxt = argv[1];
-  ASTManager manager(astFiletxt);
-#if 0
-  for (auto fd : manager.getFunctionDecls()) {
-    llvm::errs() << fd->getNameInfo().getAsString() << ": ";
-    SourceManager &sm = fd->getASTContext().getSourceManager();
-    llvm::errs() << fd->getLocation().printToString(sm) << "\n\t";
-    bool flag1 = fd->doesThisDeclarationHaveABody();
-    bool flag2 = fd->isDefined();
-    llvm::errs() << "doesThisDeclarationHaveABody: " << flag1
-                 << ", isDefined: " << flag2 << "\n";
-  }
-#endif
-  llvm::errs() << "direct function call: " << manager.getDirectCallCnt()
-               << "\n";
-  llvm::errs() << "var function ptr call: " << manager.getVarFPCnt() << "\n";
-  llvm::errs() << "parm var function ptr call: " << manager.getParmVarFPCnt()
-               << "\n";
-  llvm::errs() << "field var function ptr call: " << manager.getFieldVarFPCnt()
-               << "\n";
 
   llvm::errs() << "hello world\n";
   return 0;
